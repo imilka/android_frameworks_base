1320227f59abac78e3c8d21de4a9ce1ea8f33bd2
imilka <pkzl22@gmail.com>
2012-10-25 03:28:35 +0400

    Maybe this?

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -341,30 +341,32 @@ public class Clock extends TextView implements OnClickListener, OnLongClickListe
     }
 
     private int getColor() {
+        String curSetting = null;        
+        String curColors[] = null;
         if (ExtendedPropertiesUtils.mIsTablet) {
-            String mCurSetting = Settings.System.getString(mContext.getContentResolver(),
+           curSetting = Settings.System.getString(mContext.getContentResolver(),
                     Settings.System.NAV_BAR_COLOR);
         } else {
-            String mCurSetting = Settings.System.getString(mContext.getContentResolver(),
+            curSetting = Settings.System.getString(mContext.getContentResolver(),
                     Settings.System.STATUS_BAR_COLOR);
         }
 
         if (ExtendedPropertiesUtils.mIsTablet) {
-            String[] mCurColors = (mCurSetting == null || mCurSetting.equals("") ?
+            curColors = (curSetting == null || curSetting.equals("") ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : mCurSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_NAVBAR] : curSetting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);    
         } else {        
-            String[] mCurColors = (mCurSetting == null || mCurSetting.equals("") ?
+            curColors = (curSetting == null || curSetting.equals("") ?
                 ExtendedPropertiesUtils.PARANOID_COLORS_DEFAULTS[
-                ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR] : mCurSetting).split(
+                ExtendedPropertiesUtils.PARANOID_COLORS_STATBAR] : curSetting).split(
                 ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
         }
-        String mCurColor = mCurColors[Integer.parseInt(mCurColors[2])];
+        String curColor = curColors[Integer.parseInt(curColors[2])];
         
-        int red = new BigInteger(mCurColor.substring(2,4),16).intValue();
-        int green = new BigInteger(mCurColor.substring(4,6),16).intValue();
-        int blue = new BigInteger(mCurColor.substring(6,8),16).intValue();
+        int red = new BigInteger(curColor.substring(2,4),16).intValue();
+        int green = new BigInteger(curColor.substring(4,6),16).intValue();
+        int blue = new BigInteger(curColor.substring(6,8),16).intValue();
         int yiq = ((red*299)+(green*587)+(blue*114))/1000;
         
         return (yiq >= 128) ? Color.rgb(0, 0, 0) : Color.rgb(255, 255, 255);     
